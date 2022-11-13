e229fc6bb58176df844c0c513ac5aa08986535cb
CJ Harries <thecjharries@users.noreply.github.com>
2022-11-13 14:19:05 -0600

    Feat/add ssh key generation (#5)
    * Make provider global again
    
    * Generate an SSH key
    
    * Add output
    
    * Add note to README about TF setup
    
    * Ignore intermediate files
    
    * Add Makefile to manage everything
    
    * Generate and attach a key pair
    
    * Ignore direnv config
    
    * Give up and get LocalStack Pro trial
    
    * Add note about LS account
    
    * Create very complicated way to programmatically get on the box

--- terraform/main.ts
+++ terraform/main.ts
@@ -3,12 +3,15 @@ import { Construct } from "constructs";
 import { App, TerraformStack, TerraformOutput } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
+import { KeyPair } from "@cdktf/provider-aws/lib/key-pair";
+import { TlsProvider } from "@cdktf/provider-tls/lib/provider";
+import { PrivateKey } from "@cdktf/provider-tls/lib/private-key";
 
 export class MyStack extends TerraformStack {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        const provider = new AwsProvider(this, "aws", {
+        new AwsProvider(this, "aws", {
             region: "us-west-2",
             secretKey: "test",
             accessKey: "test",
@@ -23,14 +26,42 @@ export class MyStack extends TerraformStack {
             ]
         });
 
-        const ec2Instance = new Instance(this, "compute", {
-            ami: "ami-01456a894f71116f2",
-            instanceType: "t2.micro",
-            provider,
+        const tlsProvider = new TlsProvider(this, "null", {});
+
+        const key = new PrivateKey(this, "key", {
+            algorithm: "RSA",
+            rsaBits: 4096,
+            provider: tlsProvider,
         });
 
-        new TerraformOutput(this, "public_ip", {
-            value: ec2Instance.publicIp,
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+        const keyPair = new KeyPair(this, "keypair", {
+            keyName: "DSO Dojo 2022-11",
+            publicKey: key.publicKeyOpenssh,
+        });
+
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+        new TerraformOutput(this, "public_key", {
+            value: key.publicKeyPem,
+        });
+
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+        new TerraformOutput(this, "private_key", {
+            value: key.privateKeyPem,
+            sensitive: true,
+        });
+
+        const ec2Instance = new Instance(this, "compute", {
+            ami: "ami-000001",
+            instanceType: "t2.micro",
+            keyName: keyPair.keyName,
+        });
+
+        new TerraformOutput(this, "ec2_id", {
+            value: ec2Instance.id,
         });
     }
 }
