cb4a6228e7363211d0901063e223ab1708a32cbe
CJ Harries <thecjharries@users.noreply.github.com>
2022-11-13 12:16:27 -0600

    Feat/add baseline cdktf (#4)
    * Run cdktf init --template=typescript --local
    
    * Pull in boilerplate example and configure for LocalStack
    
    * Add basic test to ensure the CDKTF is valid and can plan

---
+++ terraform/__tests__/main-test.ts
@@ -0,0 +1,21 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import "cdktf/lib/testing/adapters/jest"; // Load types for expect matchers
+import { Testing } from "cdktf";
+import { MyStack } from "../main";
+
+describe("My CDKTF Application", () => {
+    describe("Checking validity", () => {
+        it("check if the produced terraform configuration is valid", () => {
+            const app = Testing.app();
+            const stack = new MyStack(app, "test");
+            expect(Testing.fullSynth(stack)).toBeValidTerraform();
+        });
+
+        it("check if this can be planned", () => {
+            const app = Testing.app();
+            const stack = new MyStack(app, "test");
+            expect(Testing.fullSynth(stack)).toPlanSuccessfully();
+        });
+    });
+});
