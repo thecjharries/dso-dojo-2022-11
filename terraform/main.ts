4807d5e53a617c89f52d14cc4ad198cdf1d861cc
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-13 12:05:07 -0600

    Pull in boilerplate example and configure for LocalStack

--- terraform/main.ts
+++ terraform/main.ts
@@ -1,14 +1,38 @@
-// Copyright (c) HashiCorp, Inc
-// SPDX-License-Identifier: MPL-2.0
+
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
 
-    // define resources here
-  }
+        const provider = new AwsProvider(this, "aws", {
+            region: "us-west-2",
+            secretKey: "test",
+            accessKey: "test",
+            skipCredentialsValidation: true,
+            skipMetadataApiCheck: "true",
+            skipRequestingAccountId: true,
+            s3UsePathStyle: true,
+            endpoints: [
+                {
+                    ec2: "http://localhost:4566",
+                }
+            ]
+        });
+
+        const ec2Instance = new Instance(this, "compute", {
+            ami: "ami-01456a894f71116f2",
+            instanceType: "t2.micro",
+            provider,
+        });
+
+        new TerraformOutput(this, "public_ip", {
+            value: ec2Instance.publicIp,
+        });
+    }
 }
 
 const app = new App();
