c3cf50aca50862aad52deec35b45dee30d546537
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-12 20:14:53 -0600

    Pull in Gin quick start

---
+++ main.go
@@ -0,0 +1,20 @@
+// This code taken from
+// https://github.com/gin-gonic/gin#quick-start
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
