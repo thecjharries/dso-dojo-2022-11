a1313c89a986e8036790aac833c89995f55c7e05
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-13 13:46:27 -0600

    Generate and attach a key pair

--- terraform/main.ts
+++ terraform/main.ts
@@ -3,6 +3,7 @@ import { Construct } from "constructs";
 import { App, TerraformStack, TerraformOutput } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
+import { KeyPair } from "@cdktf/provider-aws/lib/key-pair";
 import { TlsProvider } from "@cdktf/provider-tls/lib/provider";
 import { PrivateKey } from "@cdktf/provider-tls/lib/private-key";
 
@@ -33,17 +34,38 @@ export class MyStack extends TerraformStack {
             provider: tlsProvider,
         });
 
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+        const keyPair = new KeyPair(this, "keypair", {
+            keyName: "DSO Dojo 2022-11",
+            publicKey: key.publicKeyOpenssh,
+        });
+
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+        new TerraformOutput(this, "public_key", {
+            value: key.publicKeyPem,
+        });
+
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+        new TerraformOutput(this, "private_key", {
+            value: key.privateKeyPem,
+            sensitive: true,
+        });
+
         const ec2Instance = new Instance(this, "compute", {
             ami: "ami-01456a894f71116f2",
             instanceType: "t2.micro",
+            keyName: keyPair.keyName,
         });
 
         new TerraformOutput(this, "public_ip", {
             value: ec2Instance.publicIp,
         });
 
-        new TerraformOutput(this, "public_key", {
-            value: key.publicKeyPem,
+        new TerraformOutput(this, "ec2_dns", {
+            value: ec2Instance.publicDns,
         })
     }
 }
