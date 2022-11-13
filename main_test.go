a81395385a604f9cf4fbc01aa1304afe33402393
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-12 20:37:14 -0600

    Test pingHandler

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +12,16 @@ import (
 func TestGetRouter(t *testing.T) {
 	router := getRouter()
 	assert.NotNil(t, router, "The router should not be nil")
+}
+
+func TestPingHandler(t *testing.T) {
+	router := getRouter()
+	router.GET("/ping", pingHandler)
+	request := httptest.NewRequest("GET", "/ping", nil)
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code, "The response code should be 200")
+	var body map[string]string
+	json.Unmarshal(response.Body.Bytes(), &body)
+	assert.Equal(t, "pong", body["message"], "The response body should be 'pong'")
 }
