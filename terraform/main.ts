11665fe8a0a32d33d8a7852d50ac5f99bf8b232f
CJ Harries <thecjharries@users.noreply.github.com>
2022-11-19 16:42:34 -0600

    Feat/update cdktf (#11)
    * Strip Go setup
    
    * Rename class
    
    * Update CDKTF with AMI
    
    * Ensure public IP exists
    
    * Add to default security group
    
    * Move around key comments
    
    * Create new SG
    
    * Update suite name
    
    * Remove LS commands from Makefile

--- terraform/main.ts
+++ terraform/main.ts
@@ -1,42 +1,32 @@
 
 import { Construct } from "constructs";
-import { App, TerraformAsset, TerraformStack, TerraformOutput } from "cdktf";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
 import { KeyPair } from "@cdktf/provider-aws/lib/key-pair";
 import { TlsProvider } from "@cdktf/provider-tls/lib/provider";
 import { PrivateKey } from "@cdktf/provider-tls/lib/private-key";
-import * as path from "path";
+import { DataAwsAmi } from "@cdktf/provider-aws/lib/data-aws-ami";
+import { SecurityGroup } from "@cdktf/provider-aws/lib/security-group";
 
-export class MyStack extends TerraformStack {
+export class DsoDojo202211 extends TerraformStack {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
         new AwsProvider(this, "aws", {
             region: "us-west-2",
-            secretKey: "test",
-            accessKey: "test",
-            skipCredentialsValidation: true,
-            skipMetadataApiCheck: "true",
-            skipRequestingAccountId: true,
-            s3UsePathStyle: true,
-            endpoints: [
-                {
-                    ec2: "http://localhost:4566",
-                }
-            ]
         });
 
         const tlsProvider = new TlsProvider(this, "null", {});
 
+        // Don't do this in production
+        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
         const key = new PrivateKey(this, "key", {
             algorithm: "RSA",
             rsaBits: 4096,
             provider: tlsProvider,
         });
 
-        // Don't do this in production
-        // https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
         const keyPair = new KeyPair(this, "keypair", {
             keyName: "DSO Dojo 2022-11",
             publicKey: key.publicKeyOpenssh,
@@ -55,32 +45,76 @@ export class MyStack extends TerraformStack {
             sensitive: true,
         });
 
-        const userData = new TerraformAsset(this, "user_data", {
-            path: path.resolve(__dirname, "user_data.yaml"),
+        const ami = new DataAwsAmi(this, "ami", {
+            mostRecent: true,
+            owners: ["self"],
+            filter: [
+                {
+                    name: "name",
+                    values: ["dso-dojo-202211"],
+                },
+            ],
+        });
+
+        new TerraformOutput(this, "ami_id", {
+            value: ami.id,
+        });
+
+        const securityGroup = new SecurityGroup(this, "security_group", {
+            name: "DSO Dojo 2022-11",
+            description: "DSO Dojo 2022-11",
+            ingress: [
+                {
+                    protocol: "tcp",
+                    fromPort: 80,
+                    toPort: 80,
+                    cidrBlocks: ["0.0.0.0/0"]
+                },
+                {
+                    protocol: "tcp",
+                    fromPort: 22,
+                    toPort: 22,
+                    cidrBlocks: ["0.0.0.0/0"]
+                },
+                {
+                    protocol: "tcp",
+                    fromPort: 443,
+                    toPort: 443,
+                    cidrBlocks: ["0.0.0.0/0"]
+                },
+            ],
+            egress: [
+                {
+                    protocol: "-1",
+                    fromPort: 0,
+                    toPort: 0,
+                    cidrBlocks: ["0.0.0.0/0"]
+                }
+            ],
+        });
+
+        new TerraformOutput(this, "security_group_id", {
+            value: securityGroup.id,
         });
 
         const ec2Instance = new Instance(this, "compute", {
-            ami: "ami-000001",
+            ami: ami.id,
             instanceType: "t2.micro",
             keyName: keyPair.keyName,
-            userData: userData.fileName,
-        });
-
-        const goInstance = new Instance(this, "go", {
-            ami: "ami-000002",
-            instanceType: "t2.micro",
+            associatePublicIpAddress: true,
+            vpcSecurityGroupIds: [securityGroup.id],
         });
 
         new TerraformOutput(this, "ec2_id", {
             value: ec2Instance.id,
         });
 
-        new TerraformOutput(this, "go_id", {
-            value: goInstance.id,
+        new TerraformOutput(this, "ec2_public_ip", {
+            value: ec2Instance.publicIp,
         });
     }
 }
 
 const app = new App();
-new MyStack(app, "terraform");
+new DsoDojo202211(app, "terraform");
 app.synth();
