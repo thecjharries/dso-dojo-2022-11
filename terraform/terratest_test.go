67c19813722fdc333590f072609f0d904d0afcb6
CJ Harries <thecjharries@users.noreply.github.com>
2022-11-19 17:31:51 -0600

    Feat/update terratest (#12)
    * Update tests to use current CDKTF
    
    * Refactor tests to handle connection delay

--- terraform/terratest_test.go
+++ terraform/terratest_test.go
@@ -1,17 +1,14 @@
 package test
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"testing"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 
@@ -24,7 +21,7 @@ type PingResponse struct {
 	Message string `json:"message"`
 }
 
-func TestTerraformMyStack(t *testing.T) {
+func TestDsoDojo202211(t *testing.T) {
 	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "./cdktf.out/stacks/terraform", ".")
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: tempTestFolder,
@@ -32,56 +29,16 @@ func TestTerraformMyStack(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	ec2Id := terraform.Output(t, terraformOptions, "ec2_id")
-	assert.NotEmpty(t, ec2Id, "An instance should have been created")
+	defer terraform.Destroy(t, terraformOptions)
 
-	goId := terraform.Output(t, terraformOptions, "go_id")
-	assert.NotEmpty(t, goId, "An app instance should have been created")
-
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	assert.NoError(t, err, "Failed to create docker client")
-	defer cli.Close()
-
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-	assert.NoError(t, err, "Failed to list containers")
-
-	var sshPort uint16
-	var goPort uint16
-
-	for _, container := range containers {
-		for _, name := range container.Names {
-			if "/localstack-ec2."+ec2Id == name {
-				for _, port := range container.Ports {
-					if port.PrivatePort == 22 {
-						sshPort = port.PublicPort
-						break
-					}
-				}
-			}
-			if "/localstack-ec2."+goId == name {
-				for _, port := range container.Ports {
-					if port.PrivatePort == 22 {
-						goPort = port.PublicPort
-						break
-					}
-				}
-			}
-			if sshPort != 0 && goPort != 0 {
-				break
-			}
-		}
-		if sshPort != 0 && goPort != 0 {
-			break
-		}
-	}
-	logger.Log(t, "sshPort: ", sshPort)
+	ec2Ip := terraform.Output(t, terraformOptions, "ec2_public_ip")
+	assert.NotEmpty(t, ec2Ip, "An instance should have been created")
 
 	privateKey := terraform.Output(t, terraformOptions, "private_key")
 	assert.NotEmpty(t, privateKey, "A private key should exist")
 
-	host := "127.0.0.1:" + fmt.Sprint(sshPort)
-	user := "root"
+	host := ec2Ip + ":22"
+	user := "ubuntu"
 	pKey := []byte(privateKey)
 
 	signer, err := ssh.ParsePrivateKey(pKey)
@@ -95,27 +52,39 @@ func TestTerraformMyStack(t *testing.T) {
 		},
 	}
 
-	conn, err := ssh.Dial("tcp", host, conf)
-	assert.NoError(t, err, "Failed to dial")
-	defer conn.Close()
+	_, err = retry.DoWithRetryE(t, "Running SSH test", 6, 10*time.Second, func() (string, error) {
+		client, err := ssh.Dial("tcp", host, conf)
+		if err != nil {
+			return "", err
+		}
+		defer client.Close()
 
-	session, err := conn.NewSession()
-	assert.NoError(t, err, "Failed to create session")
-	defer session.Close()
+		session, err := client.NewSession()
+		assert.NoError(t, err, "Failed to create session")
+		defer session.Close()
 
-	hostname, err := session.Output("hostname")
-	assert.NoError(t, err, "Failed to get hostname")
-	logger.Log(t, "Remote hostname: ", string(hostname))
-	assert.NotEmpty(t, string(hostname), "A hostname should exist")
+		hostname, err := session.Output("hostname")
+		assert.NoError(t, err, "Failed to get hostname")
+		logger.Log(t, "Remote hostname: ", string(hostname))
+		assert.NotEmpty(t, string(hostname), "A hostname should exist")
+		return "", nil
+	})
+	assert.NoError(t, err, "Failed to SSH")
 
-	reponse, err := http.Get("http://localhost:" + fmt.Sprint(goPort) + "/ping")
-	assert.NoError(t, err, "Failed to get ping")
-	defer reponse.Body.Close()
+	_, err = retry.DoWithRetryE(t, "Running GET test", 6, 10*time.Second, func() (string, error) {
+		reponse, err := http.Get("http://" + ec2Ip + "/ping")
+		if err != nil {
+			return "", err
+		}
+		defer reponse.Body.Close()
 
-	body, err := ioutil.ReadAll(reponse.Body)
-	assert.NoError(t, err, "Failed to read response")
-	var pingResponse PingResponse
-	err = json.Unmarshal(body, &pingResponse)
-	assert.NoError(t, err, "Failed to unmarshal response")
-	assert.Equal(t, "pong", pingResponse.Message, "Ping response should be pong")
+		body, err := ioutil.ReadAll(reponse.Body)
+		assert.NoError(t, err, "Failed to read response")
+		var pingResponse PingResponse
+		err = json.Unmarshal(body, &pingResponse)
+		assert.NoError(t, err, "Failed to unmarshal response")
+		assert.Equal(t, "pong", pingResponse.Message, "Ping response should be pong")
+		return "", nil
+	})
+	assert.NoError(t, err, "Failed to hit server")
 }
